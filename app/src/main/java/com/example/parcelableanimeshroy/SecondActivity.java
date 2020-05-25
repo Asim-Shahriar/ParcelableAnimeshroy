185dd4f42266f3228c2d33fc20178abace4bf695
Asim-Shahriar <asimshariar@gmail.com>
2020-05-25 21:11:17 +0600

    SecondActivity xml java class design

--- app/src/main/java/com/example/parcelableanimeshroy/SecondActivity.java
+++ app/src/main/java/com/example/parcelableanimeshroy/SecondActivity.java
@@ -2,13 +2,32 @@ package com.example.parcelableanimeshroy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView text_name, text_age, text_phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        text_name = findViewById(R.id.name);
+        text_age = findViewById(R.id.age);
+        text_phone = findViewById(R.id.phone);
+
+        Intent intent =getIntent();
+        Details student=intent.getParcelableExtra("Information");
+
+        String name = student.getName();
+        String age = student.getAge();
+        String phone = student.getPhone();
+
+        text_name.setText("Your Name: "+name);
+        text_age.setText("Your Age: "+age);
+        text_phone.setText("Your Phone No: "+phone);
     }
 }
